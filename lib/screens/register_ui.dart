75a332d306ac57939905bac63821e4fc85d889fa
Thongek Wogponom <70640558+Thongek-wpm@users.noreply.github.com>
2022-11-21 02:50:53 -0800

    Update register_ui.dart

--- lib/screens/register_ui.dart
+++ lib/screens/register_ui.dart
@@ -24,6 +24,92 @@ class _RegisterUiState extends State<RegisterUi> {
         ),
         centerTitle: true,
       ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const <Widget>[
+          Padding(
+            padding: EdgeInsets.only(bottom: 5, top: 30, left: 10),
+            child: Text(
+              'Register Account',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              left: 20,
+              right: 40,
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                // ignore: prefer_const_constructors
+                icon: Icon(Icons.person),
+                labelText: 'Name',
+                hintText: 'Enter Name',
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              left: 20,
+              right: 40,
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                icon: Icon(Icons.perm_identity),
+                labelText: 'Username',
+                hintText: 'Enter username',
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              left: 20,
+              right: 40,
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                icon: Icon(Icons.key),
+                labelText: 'Passwords',
+                hintText: 'Enter Passwords',
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              left: 20,
+              right: 40,
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                icon: Icon(Icons.email),
+                labelText: 'E-mail',
+                hintText: 'Enter E-mail',
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              left: 20,
+              right: 40,
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                icon: Icon(Icons.phone),
+                labelText: 'Number phone',
+                hintText: 'Enter Number phone',
+              ),
+            ),
+          ),
+          // ElevatedButton(
+          //   onPressed: () {},
+          //   child: Text('Done'),
+          // ),
+        ],
+      ),
     );
   }
 }

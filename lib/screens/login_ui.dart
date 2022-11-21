a9f18ef1d6a29c15cdcc0f75d64806b4e6a89286
Thongek Wogponom <70640558+Thongek-wpm@users.noreply.github.com>
2022-11-21 02:13:59 -0800

    add&updata registerUi

--- lib/screens/login_ui.dart
+++ lib/screens/login_ui.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, duplicate_ignore, unused_import, prefer_typing_uninitialized_variables
 import 'package:flutter/material.dart';
 import 'package:flutter_application_foods/screens/forgot_ui.dart';
+import 'package:flutter_application_foods/screens/register_ui.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter_application_foods/src/conroller.dart';
 
@@ -12,7 +13,6 @@ class LoginUi extends StatefulWidget {
 }
 
 class _LoginUiState extends State<LoginUi> {
-
   @override
   Widget build(BuildContext context) {
     var usernameCon;
@@ -82,7 +82,14 @@ class _LoginUiState extends State<LoginUi> {
                 ),
               ),
               TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const RegisterUi(),
+                    ),
+                  );
+                },
                 child: Text(
                   "Register Here!!",
                   style: TextStyle(
---
+++ lib/screens/register_ui.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class RegisterUi extends StatefulWidget {
+  const RegisterUi({super.key});
+
+  @override
+  State<RegisterUi> createState() => _RegisterUiState();
+}
+
+class _RegisterUiState extends State<RegisterUi> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: const Text(
+          "Register Account User",
+          style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+    );
+  }
+}

94a5d176419cfb57589c8b98eac127fe75c14c07
Thongek Wogponom <70640558+Thongek-wpm@users.noreply.github.com>
2022-11-21 15:20:00 -0800

    Update forgot_ui.dart

--- lib/screens/forgot_ui.dart
+++ lib/screens/forgot_ui.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
+import 'package:flutter_application_foods/screens/home_ui.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ForgotUi extends StatefulWidget {
@@ -9,18 +12,60 @@ class ForgotUi extends StatefulWidget {
 }
 
 class _ForgotUiState extends State<ForgotUi> {
+
+  final TextEditingController emailConroller = TextEditingController(text: '');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
           onPressed: () {
             Navigator.pop(context);
           },
-          icon: const Icon(Icons.arrow_back_ios),
         ),
-        title: const Text("Forget Passwords"),
+        title: const Text(
+          "Forget your password",
+          style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const <Widget>[
+          Padding(
+            padding: EdgeInsets.only(
+              left: 40,
+              right: 40,
+            ),
+            child: Text(
+              "You are Forget your password?",
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              left: 30,
+              right: 30,
+              top: 5,
+            ),
+            child: TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                icon: Icon(Icons.email),
+                labelText: 'E-mail',
+                hintText: 'Enter E-mail',
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(40),
+          )
+        ],
       ),
     );
   }

368d72df107ef8d50748821f3492bc22c4b3b969
Thongek Wogponom <70640558+Thongek-wpm@users.noreply.github.com>
2022-11-21 03:55:14 -0800

    Update conroller.dart

--- lib/src/conroller.dart
+++ lib/src/conroller.dart
@@ -4,5 +4,9 @@ class Conroller {
   final TextEditingController usernameCon = TextEditingController(text: '');
   final TextEditingController passwordCon = TextEditingController(text: '');
   final TextEditingController emailCon = TextEditingController(text: '');
-
+  final TextEditingController editEmailCon = TextEditingController(text: '');
+  final TextEditingController editPasswordCon = TextEditingController(text: '');
+  final TextEditingController editMobilelCon = TextEditingController(text: '');
+  final emailReexprssion =
+      RegExp('[A-Z0-9a-z.%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}');
 }

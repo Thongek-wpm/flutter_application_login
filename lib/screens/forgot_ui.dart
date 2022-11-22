8538d234f394b79eba5a95af6a2340613c779ff7
Thongek Wogponom <70640558+Thongek-wpm@users.noreply.github.com>
2022-11-21 16:27:13 -0800

    Update forgot_ui.dart

--- lib/screens/forgot_ui.dart
+++ lib/screens/forgot_ui.dart
@@ -32,19 +32,28 @@ class _ForgotUiState extends State<ForgotUi> {
         centerTitle: true,
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Text(
-              "YOU ARE FOGRET YOUR PASSWORD?",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(5),
+                child: Text(
+                  "YOU ARE FOGRET YOUR PASSWORD?",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
-            ),
+            ],
           ),
           Padding(
-            padding: EdgeInsets.only(right: 40, left: 40, top: 10),
+            padding: EdgeInsets.all(25),
             child: TextField(
               controller: emailCon,
               keyboardType: TextInputType.emailAddress,
@@ -56,13 +65,19 @@ class _ForgotUiState extends State<ForgotUi> {
               ),
             ),
           ),
-          Padding(
-            padding: EdgeInsets.all(0.5),
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text('Okay'),
-            ),
-          )
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(0.1),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Send Password'),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
